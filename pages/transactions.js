77b8f420a5969dd0a258caf2e40828bc5ab8c40e
Daniel Cronqvist <dancro99@gmail.com>
2021-05-18 22:29:30 +0200

    Added datepicker

--- pages/transactions.js
+++ pages/transactions.js
@@ -2,13 +2,17 @@ import Head from 'next/head'
 import styles from '../styles/Transactions.module.css'
 import TransactionView from '../components/TransactionView'
 import { useState } from 'react';
+import DatePicker from 'react-datepicker'
 const axios = require('axios').default;
 
-const token = "x"
+const token = "c60f9280-921c-4bf6-872d-af8d9dadf12c"
 
 export default function Transactions() {
     const [transactions, setTransactions] = useState([])
     const [accounts, setAccounts] = useState({})
+    const [filterMode, setFilterMode] = useState("")
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
 
     axios.get("https://devapi.dcronqvist.se/v1/economy/periods/months?token=" + token + "&username=dani&year=2020&month=5").then(response => {   
         const startDate = response.data.response[0].start_timestamp
@@ -30,9 +34,26 @@ export default function Transactions() {
 			</Head>
             
             <main className={styles.main}>
-                <h1>
-                    All transactions
-                </h1>
+                <div className={styles.head}>
+                    <h1>
+                        All transactions
+                    </h1>
+
+                    { filterMode == "customRange" ? 
+                        <>
+                            <DatePicker isClearable placeholderText="Starting date" selected={startDate} onChange={(date) => setStartDate(date)}/>
+                        </>
+                        :
+                        <>
+
+                        </>
+                    }
+
+                    <select value={filterMode} name="filterMode" id="filterMode" onChange={(e) => setFilterMode(e.target.value)}>
+                        <option value="currentMonth">current month</option>
+                        <option value="customRange">custom range</option>
+                    </select>
+                </div>  
                 <div className={styles.list}>
                     {   
                         transactions.map(trans => {
