81dd87476a1773c2a201790b4cb25f4834ee8567
Daniel Cronqvist <dancro99@gmail.com>
2021-05-18 21:49:55 +0200

    Added some stuff to transactions page

--- components/TransactionView.js
+++ components/TransactionView.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/TransactionView.module.css'
 import cn from 'classnames'
 import { useState } from 'react'
+import { format } from 'date-fns'
 
 /*
 expects props with a transaction field which looks like this:
@@ -19,53 +20,33 @@ export default function TransactionView(props) {
 
     const [editing, setEditing] = useState(false)
 
-    const date = props.transaction.date_trans
+    const date = new Date(props.transaction.date_trans * 1000)
     const amount = props.transaction.amount
     const from_account = props.transaction.from_account
     const to_account = props.transaction.to_account
     const desc = props.transaction.desc
 
     let div = <div className={styles.container}>      
-                    <span className={cn(styles.item)}>
-                        {date}
+                    <span className={cn(styles.item, styles.small)}>
+                        {format(date, "yyyy-MM-dd hh:MM")}
                     </span>
-                    <span className={cn(styles.item)}>
+                    <span className={cn(styles.item, styles.small)}>
                         {from_account} &rarr; {to_account}
                     </span>
-                    <span className={cn(styles.item)}>
+                    <span className={cn(styles.item, styles.medium)}>
                         {amount} kr
                     </span>
                     <span className={cn(styles.item, styles.big)}>
                         {desc}
                     </span>
-                    <span className={cn(styles.item, styles.medium)}>
-                        <input type="button" value="Edit" onClick={(e) => {setEditing(true)}}/>
+                    <span className={cn(styles.item, styles.medium, styles.right)}>
+                        <input type="button" value="Edit" onClick={(e) => {}}/>
                         <input type="button" value="Delete" onClick={(e) => {}}/>
                     </span>
                 </div>
     
     // TODO: Make account, amount and desc fields text
     // inputs when editing is true.
-    if (editing) {
-        div = <div className={styles.container}>      
-                <span className={cn(styles.item)}>
-                    {date}
-                </span>
-                <span className={cn(styles.item)}>
-                    {from_account} &rarr; {to_account}
-                </span>
-                <span className={cn(styles.item)}>
-                    {amount} kr
-                </span>
-                <span className={cn(styles.item, styles.big)}>
-                    {desc}
-                </span>
-                <span className={cn(styles.item, styles.medium)}>
-                    <input type="button" value="Save" onClick={(e) => {setEditing(false)}}/>
-                </span>
-            </div>
-    }
-
 
     return div;
 }
--- pages/transactions.js
+++ pages/transactions.js
@@ -1,8 +1,28 @@
 import Head from 'next/head'
 import styles from '../styles/Transactions.module.css'
 import TransactionView from '../components/TransactionView'
+import { useState } from 'react';
+const axios = require('axios').default;
+
+const token = "x"
 
 export default function Transactions() {
+    const [transactions, setTransactions] = useState([])
+    const [accounts, setAccounts] = useState({})
+
+    axios.get("https://devapi.dcronqvist.se/v1/economy/periods/months?token=" + token + "&username=dani&year=2020&month=5").then(response => {   
+        const startDate = response.data.response[0].start_timestamp
+        const endDate = response.data.response[0].end_timestamp
+
+        axios.get('https://devapi.dcronqvist.se/v1/economy/transactions?token=' + token + '&username=dani&startDate=' + startDate + '&endDate=' + endDate).then(response => {
+            setTransactions(response.data.response)
+        }) 
+    }).catch((error) => {
+        if(error.response) {
+            console.log(error.response.data)
+        }
+    })
+
     return (
         <div className={styles.container}>
             <Head>
@@ -13,22 +33,13 @@ export default function Transactions() {
                 <h1>
                     All transactions
                 </h1>
-                <TransactionView transaction={{
-                    date_reg: 1571219671.024,
-                    date_trans: 1571176800,
-                    amount: 175,
-                    desc: "Fysikbok",
-                    from_account: 101,
-                    to_account: 408
-                }}/>
-                <TransactionView transaction={{
-                    date_reg: 1571219671.024,
-                    date_trans: 1571176800,
-                    amount: 1750,
-                    desc: "Fysikbok",
-                    from_account: 101,
-                    to_account: 408
-                }}/>
+                <div className={styles.list}>
+                    {   
+                        transactions.map(trans => {
+                            return <TransactionView key={trans._id} transaction={trans}/>
+                        })
+                    }   
+                </div>             
             </main>
         </div>
     )
