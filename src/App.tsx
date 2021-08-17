90bcf16410a65bf73a12d7a549166f750c511eb3
Daniel Cronqvist <dancro99@gmail.com>
2021-08-17 23:47:15 +0200

    Tried out using react-routers Link efficiently

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import LoginPage from "src/pages/Login"
 import { useAuthorization } from "./contexts/AuthorizationContext"
 
@@ -14,11 +14,27 @@ function App(): JSX.Element {
         <LoginPage />
       ) : (
         <Router>
+          <h1>Authorized</h1>
+          <Link to="/">dashboard</Link>
+          <br />
+          <Link to="/accounts">accounts</Link>
+          <br />
+          <Link to="/transactions">transactions</Link>
+          <br />
           <div>
             {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-            <Switch></Switch>
-            <h1>Authorized</h1>
+            <Switch>
+              <Route exact path="/">
+                <h1>dashboard</h1>
+              </Route>
+              <Route path="/accounts">
+                <h1>accounts</h1>
+              </Route>
+              <Route path="/transactions">
+                <h1>transactions</h1>
+              </Route>
+            </Switch>
           </div>
         </Router>
       )}
